b68e2160ae6c4eb0b14528943ae99a621ddf8b9d
alexandra.harko <sanyuta93@gmail.com>
2025-06-19 22:34:49 +0300

    Fixed build error

--- Lesson2/src/zeroOne.java
+++ Lesson2/src/zeroOne.java
@@ -1,5 +1,6 @@
 public class zeroOne {
-    public static void zeroOne (int[] arr) {
+
+    public static void zeroOne(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
                 arr[i] = 1;
@@ -7,12 +8,7 @@ public class zeroOne {
                 arr[i] = 0;
             }
 
+        }
     }
 
-        public static void  {
-            for (int num : arr) {
-                System.out.print(num + " ");
-            }
-            System.out.println();
-        }
 }
