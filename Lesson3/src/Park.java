8a5ef443fdfaed8bcbde0dc295a3872cc2ca4f9a
alexandra.harko <sanyuta93@gmail.com>
2025-06-25 22:25:56 +0300

    edit the task Park

--- Lesson3/src/Park.java
+++ Lesson3/src/Park.java
@@ -1,104 +1,59 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-
-
 public class Park {
-    private String parkName;
-    private List<Attraction> attractions;
 
-
-    public class Attraction {
+    public static class Attraction {
         private String name;
-        private Map<String, String> workingHours;
+        private String workTime;
         private double price;
 
-        public Attraction(String name, double price) {
+
+        public Attraction(String name, String workTime, double price) {
             this.name = name;
+            this.workTime = workTime;
             this.price = price;
-            this.workingHours = new HashMap<>();
         }
 
-        public void setWorkingHours(String day, String hours) {
-            workingHours.put(day, hours);
-        }
-
-        public String getWorkingHours(String day) {
-            return workingHours.getOrDefault(day, "Выходной");
-        }
 
         public String getName() {
             return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getWorkTime() {
+            return workTime;
+        }
+
+        public void setWorkTime(String workTime) {
+            this.workTime = workTime;
         }
 
         public double getPrice() {
             return price;
         }
 
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+
         @Override
         public String toString() {
-            return String.format("%s - Цена: %.2f руб.", name, price);
+            return "Attraction: " + name +
+                    ", Work Time: " + workTime +
+                    ", Price: " + price + " USD";
         }
     }
 
-    public Park(String parkName) {
-        this.parkName = parkName;
-        this.attractions = new ArrayList<>();
-    }
-
-    public void addAttraction(String name, double price) {
-        Attraction newAttraction = new Attraction(name, price);
-        attractions.add(newAttraction);
-    }
-
-    public Attraction getAttraction(String name) {
-        for (Attraction attraction : attractions) {
-            if (attraction.getName().equals(name)) {
-                return attraction;
-            }
-        }
-        return null;
-    }
-
-    public void printAllAttractions() {
-        System.out.println("Аттракционы парка '" + parkName + "':");
-        for (Attraction attraction : attractions) {
-            System.out.println(attraction);
-            System.out.println("  Режим работы:");
-            System.out.println("  Пн: " + attraction.getWorkingHours("Monday"));
-            System.out.println("  Вт: " + attraction.getWorkingHours("Tuesday"));
-            System.out.println("  Ср: " + attraction.getWorkingHours("Wednesday"));
-            System.out.println("  Чт: " + attraction.getWorkingHours("Thursday"));
-            System.out.println("  Пт: " + attraction.getWorkingHours("Friday"));
-            System.out.println("  Сб: " + attraction.getWorkingHours("Saturday"));
-            System.out.println("  Вскр: " + attraction.getWorkingHours("Sunday"));
-        }
-    }
 
     public static void main(String[] args) {
-        Park disneyland = new Park("Disneyland");
 
+        Attraction attraction1 = new Attraction("Waltz", "10:00 - 20:00", 5.5);
+        Attraction attraction2 = new Attraction("Ferris Wheel", "09:00 - 22:00", 8.0);
 
-        disneyland.addAttraction("Space Mountain", 500);
-        disneyland.addAttraction("Pirates of the Caribbean", 350);
-        disneyland.addAttraction("Haunted Mansion", 400);
-
-
-        Attraction spaceMountain = disneyland.getAttraction("Space Mountain");
-        if (spaceMountain != null) {
-            spaceMountain.setWorkingHours("Monday", "10:00-20:00");
-            spaceMountain.setWorkingHours("Tuesday", "10:00-20:00");
-            spaceMountain.setWorkingHours("Saturday", "9:00-22:00");
-        }
-
-        Attraction pirates = disneyland.getAttraction("Pirates of the Caribbean");
-        if (pirates != null) {
-            pirates.setWorkingHours("Monday", "11:00-19:00");
-            pirates.setWorkingHours("Sunday", "10:00-21:00");
-        }
-
-        disneyland.printAllAttractions();
+        // Выводим информацию о них
+        System.out.println(attraction1);
+        System.out.println(attraction2);
     }
 }
